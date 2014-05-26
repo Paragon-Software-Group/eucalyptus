f020a65f7fd1d1dec0744a81c4f1dc5015698fb5
feoff <feoff3@gmail.com>
2014-05-26 21:53:01 +0400

    fixed build

--- clc/modules/msgs/src/main/java/com/eucalyptus/ws/server/NioServerHandler.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/ws/server/NioServerHandler.java
@@ -132,8 +132,6 @@ public class NioServerHandler extends SimpleChannelUpstreamHandler {//TODO:GRZE:
       ctx.sendUpstream( e );
     } catch ( Exception ex ) {
       LOG.error( ex , ex );
-      ex.printStackTrace();
-      ex.printStackTrace(LOG);
       Logs.extreme( ).error( ex, ex );
       stat.call( );
       this.sendError( ctx, HttpResponseStatus.NOT_FOUND, ex );
