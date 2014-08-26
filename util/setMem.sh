2504f25f6bca7d12950282ee3587974b0df60ea9
U-PARAGON\mironov2 <mironov2@SB654.paragon-software.com>
2014-08-26 14:21:40 +0400

    Added script to easy change ram quota for eucalyptus launch

---
+++ util/setMem.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+[[ ! -n $0 ]];
+then
+	memJava=$0
+else
+	memJava="2048m"
+fi
+cat /etc/eucalyptus/eucalyptus.conf | sed "s/CLOUD_OPTS=\".*\"/CLOUD_OPTS=\"$memJava\"/">/etc/eucalyptus/eucalyptus.conf
